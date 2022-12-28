fc1ef6c98d3a281021518ab76d98a3a9b968d2e2
Abdelrahman Hebeish <abdelrahman.hepish2001@gmail.com>
2022-12-28 22:53:28 +0200

    Creating Foundation Directory

---
+++ 01_Foundation/02_Introduction_to_C++/05_Send Output to the Console/solution.cpp
@@ -0,0 +1,19 @@
+/* 
+Previously, you wrote the most basic program that will compile in C++: a simple main(). In this exercise, you will modify that program to print output to the terminal. Don't forget that you can compile and run your program with the following two steps from the command line:
+
+    g++ main.cpp
+    ./a.out
+
+To Complete This Exercise:
+
+        Add code to the main.cpp file below to print "Hello!" using cout.
+
+*/
+#include <iostream>
+using std::cout;
+
+int main() {
+  // TODO: Add your code here:
+  cout<<"Hello!\n" ; 
+  
+}
