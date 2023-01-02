60fb2c49655b7c284cf441373be05e3d755d86fc
Abdelrahman Hebeish <abdelrahman.hepish2001@gmail.com>
2023-01-02 22:16:04 +0200

    Using auto section

---
+++ 01_Foundation/12_Using_auto/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std ; 
+using std::vector;
+using std::cout;
+
+int main() {
+    auto i = 5;
+    auto v_6 = {1, 2, 3};
+    //cout<<typeid(i)<< endl;
+    //#cout<<typeid(v_6) << endl;
+    cout << "Variables declared and initialized without explicitly stating type!" << "\n";
+    }
